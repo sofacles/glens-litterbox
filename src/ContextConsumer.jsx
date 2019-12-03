861cd5e1892e6b56b31d566bc76e191e6b1abe9b
steve <sofacles@yahoo.com>
2019-12-03 15:07:40 -0800

    useContext can return an arbitrary array of objects

--- src/ContextConsumer.jsx
+++ src/ContextConsumer.jsx
@@ -2,8 +2,9 @@ import React, {useContext} from "react";
 import {SrcContext} from "./SrcContext"
 
 const ContextConsumer = (props) => {
-    const [state, setState] = useContext(SrcContext);
-    return <div> I am inside the provider {state.src}</div>
+    const [state, Increment, Decrement] = useContext(SrcContext);
+    return <div> I am inside the provider {state.src}. 
+    <p>  <button onClick={Decrement}>Decrement</button> &nbsp;  <button onClick={Increment}>Increment</button></p></div>
 };
 
 
--- src/SrcContext.js
+++ src/SrcContext.js
@@ -1,12 +1,37 @@
-import React, {createContext, useState } from "react";
+import React, { createContext, useState, useRef } from "react";
 
 const SrcContext = createContext([{}, () => {}]);
 
-const SrcContextProvider = (props) => {
-    const [state, setState ] = useState({src: "a.jpg"});
-    return <SrcContext.Provider value={[state, setState]}>
-        {props.children}
-    </SrcContext.Provider>
-}
+const SrcContextProvider = props => {
+  const [state, setState] = useState({ src: "a.jpg" });
+  const index = useRef(0);
+  const images = ["a.jpg", "b.jpg", "c.jpg"];
 
-export {SrcContext, SrcContextProvider};
+  const IncrementIndex = () => {
+    const length = images.length;
+    if (index.current < length - 1) {
+      index.current = index.current + 1;
+    } else {
+      index.current = 0;
+    }
+    
+    setState({ src: images[index.current] });
+  };
+
+  const DecrementIndex = () => {
+    if (index.current === 0) {
+      index.current = images.length - 1;
+    } else {
+      index.current = index.current - 1;
+    }
+    
+    setState({ src: images[index.current] });
+  };
+  return (
+    <SrcContext.Provider value={[state, IncrementIndex, DecrementIndex]}>
+      {props.children}
+    </SrcContext.Provider>
+  );
+};
+
+export { SrcContext, SrcContextProvider };
