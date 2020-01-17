c3ee11337f00fa57fb169a541dcfcd3530f60811
steve <sofacles@yahoo.com>
2020-01-17 10:44:14 -0800

    adding api endpoint, proxied by CRA

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/healthcheck/", async (req, res) => {
+  const blob = { ok: "dokey", colors: ["red", "green"] };
+  res.send(blob);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { connect } from "react-redux"
 import './App.css';
+import axios from "axios";
 
 const App = (props) => {
+ const [colors, setColors] = useState([]);
+  useEffect(() => {
+    debugger;
+    axios.get("/api/healthcheck")
+    .then( (response) => {
+      debugger;
+      setColors(response.data.colors);
+    })
+  }, [])
   
+  let colorElements = colors.map(c => (<li>{c}</li>));
   return (
     <div className="App">
       {props.red ? "red" : "not red"} , {props.green ? "green" : "not green"}
       <div>
         <button onClick={props.makeGreen}>Make Green</button>
       </div>
+    <ul>{colorElements}</ul>
+
     </div>
   );
 }
